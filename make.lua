22d4bb6a85ef5b121337d1679d370da3b2a828e2
Xiangdong Zeng <xdzeng96@gmail.com>
2018-09-01 20:12:37 +0800

    update class and lua script
    FAQs without tags will be numbered automatically.

--- make.lua
+++ make.lua
@@ -32,6 +32,7 @@ aux_files  =
         "synctex.gz",
         "xdv"
     }
+minted_dir = "_minted-" .. job_name
 
 function get_path(...)
     local sep
@@ -50,6 +51,14 @@ function get_path(...)
     return result
 end
 
+-- function cwd()
+--     if os.type == "windows" then
+--         return io.popen("CD"):read()
+--     else
+--         return io.popen("pwd"):read()
+--     end
+-- end
+
 function rm(file)
     if os.type == "windows" then
         return os.execute("DEL /Q " .. file)
@@ -58,23 +67,46 @@ function rm(file)
     end
 end
 
+function rmdir(dir)
+    if os.type == "windows" then
+        return os.execute("RMDIR /S /Q " .. dir)
+    else
+        return os.execute("rm -rf " .. dir)
+    end
+end
+
+function latexmk(file, arg)
+    return "latexmk " .. arg .. " " .. file
+end
+
+function build(arg)
+    latexmk_arg = arg or "-xelatex -shell-escape"
+    cmd = "cd " .. get_path(".", source_dir) .. " && " .. latexmk(job_name, latexmk_arg)
+    os.execute(cmd)
+end
+
 function clean()
     for _, i in ipairs(aux_files) do
         rm(get_path(".", source_dir, "*." .. i))
     end
+    rmdir(get_path(".", source_dir, minted_dir))
 end
 
 function help()
     local help_info =
         "\n" ..
         "  Lua build script for latex-faq-cn  Copyright (C) 2018 by latexstudio\n\n" ..
-        "Usage:\n" ..
-        "  -c, --clean    Delete auxiliary files\n" ..
-        "  -h, --help     Print this help message then exit"
+        "Usage: " .. arg[0] .. " [-b [arg]|-c|-h]\n\n" ..
+        "  -b, --build    Build latex-faq-cn with latexmk.\n" ..
+        "                 Can take an optional argument.\n\n" ..
+        "  -c, --clean    Delete auxiliary files.\n\n" ..
+        "  -h, --help     Print this help message then exit."
     return print(help_info)
 end
 
-if arg[1] == "-c" or arg[1] == "--clean" then
+if arg[1] == "-b" or arg[1] == "--build" then
+    return build(arg[2])
+elseif arg[1] == "-c" or arg[1] == "--clean" then
     return clean()
 elseif arg[1] == "-h" or arg[1] == "--help" then
     return help()
